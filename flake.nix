926b4e43f3faad345db39c0340fb2c563b289c6a
Alex Caza <alex@alexcaza.com>
2024-03-05 21:15:59 -0500

    Format

--- flake.nix
+++ flake.nix
@@ -9,14 +9,18 @@
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = inputs@{ self, nix-darwin, nixpkgs, home-manager }:
-  let
-    configuration = { pkgs, ... }: {
+  outputs = inputs @ {
+    self,
+    nix-darwin,
+    nixpkgs,
+    home-manager,
+  }: let
+    configuration = {pkgs, ...}: {
       # List packages installed in system profile. To search by name, run:
       # $ nix-env -qaP | grep wget
-      environment.systemPackages =
-        [ pkgs.vim
-        ];
+      environment.systemPackages = [
+        pkgs.vim
+      ];
 
       # Auto upgrade nix package and the daemon service.
       services.nix-daemon.enable = true;
@@ -26,7 +30,7 @@
       nix.settings.experimental-features = "nix-command flakes";
 
       # Create /etc/zshrc that loads the nix-darwin environment.
-      programs.zsh.enable = true;  # default shell on catalina
+      programs.zsh.enable = true; # default shell on catalina
       # programs.fish.enable = true;
 
       # Set Git commit hash for darwin-version.
@@ -39,25 +43,24 @@
       # The platform the configuration will be used on.
       nixpkgs.hostPlatform = "aarch64-darwin";
     };
-  in
-  {
+  in {
     # Build darwin flake using:
     # $ darwin-rebuild build --flake .#Countess-Sparkles
     darwinConfigurations."Countess-Sparkles" = nix-darwin.lib.darwinSystem {
-      modules = [ 
-	configuration 
-	home-manager.darwinModules.home-manager
+      modules = [
+        configuration
+        home-manager.darwinModules.home-manager
         {
-	  users.users.alexcaza.home = "/Users/alexcaza";
+          users.users.alexcaza.home = "/Users/alexcaza";
           home-manager.useGlobalPkgs = true;
           home-manager.useUserPackages = true;
           home-manager.users.alexcaza = {
-imports = [
-./home.nix
-];
-};
-	}
-	];
+            imports = [
+              ./home.nix
+            ];
+          };
+        }
+      ];
     };
 
     # Expose the package set, including overlays, for convenience.
--- home.nix
+++ home.nix
@@ -76,5 +76,4 @@
   imports = [
     ./home
   ];
-
 }

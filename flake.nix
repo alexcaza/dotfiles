109419f8156e789a399f9a2af367d9ffcf6804e6
Alex Caza <alex@alexcaza.com>
2024-03-25 21:36:09 -0400

    Add weztermocil using an overlay
    This pulls directly from the Github repo for now. Eventually, once the tool is--hopefully--published to Nixpkgs, I'll remove the overlay and use that intead.

--- flake.nix
+++ flake.nix
@@ -9,18 +9,30 @@
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = inputs @ {
+  outputs = {
     self,
     nix-darwin,
     nixpkgs,
     home-manager,
-  }: {
+  } @ inputs: {
     # Build darwin flake using:
     # $ darwin-rebuild build --flake .#Countess-Sparkles
     darwinConfigurations."Countess-Sparkles" = nix-darwin.lib.darwinSystem {
       modules = [
         home-manager.darwinModules.home-manager
         {
+          nixpkgs.overlays = [
+            (self: super: let
+              src = super.fetchFromGitHub {
+                owner = "alexcaza";
+                repo = "weztermocil";
+                rev = "320ec85194c21223dace0851343b7295d37ba1ba";
+                hash = "sha256-lGPwvZcjLjlyC/aav1xLVAOqieMDnpXCr79mV+NUvy4=";
+              };
+            in {
+              weztermocil = super.callPackage src {};
+            })
+          ];
           users.users.alexcaza.home = "/Users/alexcaza";
           home-manager.useGlobalPkgs = true;
           home-manager.useUserPackages = true;

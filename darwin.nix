3166e5586a164e4024301eeb5d02c9466410fa33
Alex Caza <alex@alexcaza.com>
2024-03-26 15:36:53 -0400

    Enable homebrew

--- darwin.nix
+++ darwin.nix
@@ -8,6 +8,10 @@
     pkgs.vim
   ];
 
+  homebrew = {
+    enable = true;
+  };
+
   # Auto upgrade nix package and the daemon service.
   services.nix-daemon.enable = true;
   # nix.package = pkgs.nix;

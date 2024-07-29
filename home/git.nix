388ecadd90d96b881db45bd42284b8a2fdb6ce7a
Alex Caza <alex@alexcaza.com>
2024-07-29 10:17:29 -0400

    Add alias to delete pruned branches

--- home/git.nix
+++ home/git.nix
@@ -2,6 +2,13 @@
   programs.git = {
     enable = true;
 
+    aliases = {
+      gone = "!git fetch -p && \
+              git for-each-ref --format '%(refname:short) %(upstream:track)' | \
+              awk '$2 == \"[gone]\" {print $1}' | \
+              xargs -r git branch -D";
+    };
+
     extraConfig = {
       user = {
         name = "Alex Caza";

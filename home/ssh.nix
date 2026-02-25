89d95b73ffb5c8aa2e3e76b18ccfd5add769b6bc
Alex Caza <alex@alexcaza.com>
2026-02-25 13:05:00 -0500

    Fix ssh config

--- home/ssh.nix
+++ home/ssh.nix
@@ -1,24 +1,28 @@
 {
   programs.ssh = {
     enable = true;
-    addKeysToAgent = "yes";
+    enableDefaultConfig = false;
 
     matchBlocks = {
       github = {
         host = "github.com";
         identityFile = "~/.ssh/github_ssh";
+        addKeysToAgent = "yes";
       };
       gitlab = {
         host = "gitlab.com";
         identityFile = "~/.ssh/gitlab_ssh";
+        addKeysToAgent = "yes";
       };
       huggingface = {
         host = "huggingface.co";
         identityFile = "~/.ssh/huggingface";
+        addKeysToAgent = "yes";
       };
       hf = {
         host = "hf.co";
         identityFile = "~/.ssh/huggingface";
+        addKeysToAgent = "yes";
       };
     };
   };
